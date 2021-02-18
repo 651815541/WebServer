5a5662c1325420ce7f73488ad005e85fb5540928
651815541 <651815541@qq.com>
2021-02-18 19:53:44 +0800

    2021.02.18

---
+++ V6/src/main/java/com/webserver/http/HttpResponse.java
@@ -0,0 +1,81 @@
+package com.webserver.http;
+
+import java.io.*;
+import java.net.Socket;
+
+public class HttpResponse {
+    //现发送一个固定的页面，测试浏览器是否可以正常接收
+    private File file = new File("./webapps/myweb/index.html");
+    private Socket socket;
+    public HttpResponse(Socket socket){
+        this.socket=socket;
+        String line = "HTTP/1.1 200 OK";
+        sendResponseline(line);
+        line = "Content-Type: text/html";
+        sendResponseContent(line);
+        line = "Content-Length: "+file.length();
+        sendResponseContent(line,true);
+        sendRespinseData();
+
+    }
+    private void sendResponseline(String line){
+        try {
+            write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void sendResponseContent(String line){
+
+        try {
+            write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void sendResponseContent(String line,boolean bool){
+
+        try {
+            write(line,bool);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private void sendRespinseData() {
+        try {
+            write();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void write(String line) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        byte[] data = line.getBytes("ISO8859-1");
+        out.write(data);
+        out.write(13);//单独发送回车符
+        out.write(10);//单独发送换行符
+    }private void write(String line,boolean bool) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        byte[] data = line.getBytes("ISO8859-1");
+        out.write(data);
+        out.write(13);//单独发送回车符
+        out.write(10);//单独发送换行符
+        if(bool==true){
+            out.write(13);//单独发送回车符
+            out.write(10);
+        }
+    }
+    private void write() throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+        int len;
+        byte[] buf=new byte[1024*10];
+        while((len=fis.read(buf))!=-1){
+            out.write(buf,0,len);
+        }
+    }
+
+
+}
